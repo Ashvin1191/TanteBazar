7aeeff0638afa717d152cb1636c8022d50c686ee
AshvinEttoo <ashvinettoo11@gmail.com>
2019-02-19 08:11:29 +0400

    Added client code

---
+++ example/TanteBazar.Example.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TanteBazar.WebApi.Client;
+
+namespace TanteBazar.Example.Console
+{
+    class Program
+    {
+        public static Client ApiClient;
+
+        static void Main(string[] args)
+        {
+            ApiClient = new Client(new Configuration("http://localhost:5000", "D32EB8F6-12FB-4BD0-BCFD-0607FD99AA97"));
+
+            System.Console.WriteLine("Getting the list of items from the API using the client");
+            var items = ApiClient.GetItemsAsync().Result;
+
+            int id = 0;
+            foreach(var item in items)
+            {
+                id++;
+
+                System.Console.WriteLine($"ItemId: {id}, ItemName: {item.Name}, ItemDescription: {item.Description}, ItemUnitPrice:{item.UnitPrice}");
+                System.Console.WriteLine();
+            }
+
+            System.Console.WriteLine("Getting this user's basket from the API using the client");
+            var basket = ApiClient.GetBasketAsync().Result;
+
+            System.Console.WriteLine($"Total item in basket: {basket.TotalItems}, TotalPrice: {basket.TotalPrice}");
+            foreach(var basketItem in basket.Items)
+            {
+                System.Console.WriteLine($"{basketItem.Quantity} x {basketItem.ItemName} = {basketItem.UnitPrice}");
+            }
+
+            System.Console.ReadKey();
+        }
+    }
+}
--- src/TanteBazar.WebApi.Client/Client.cs
+++ src/TanteBazar.WebApi.Client/Client.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using TanteBazar.WebApi.Client.Models;
 
 namespace TanteBazar.WebApi.Client
 {
@@ -22,14 +23,41 @@ namespace TanteBazar.WebApi.Client
         public async Task<List<Models.Item>> GetItemsAsync()
         {
             _httpClient.DefaultRequestHeaders.Add("X_API_SECRET", _config.ClientSecret);
+            
 
-            var resultString = await _httpClient.GetStringAsync($"{_config.ServiceUrl}/items");
+            var resultString = "";
 
-            if (string.IsNullOrEmpty(resultString))
-                return null;
+            using (var httpResponse = await _httpClient.GetAsync($"{_config.ServiceUrl}/api/items"))
+            {
+                resultString = await httpResponse.Content.ReadAsStringAsync();
+            }
+
+                if (string.IsNullOrEmpty(resultString))
+                    return null;
 
             var result = JsonConvert.DeserializeObject<List<Models.Item>>(resultString);
 
             return result;
+        }
+
+        public async Task<Basket> GetBasketAsync()
+        {
+            _httpClient.DefaultRequestHeaders.Add("X_API_SECRET", _config.ClientSecret);
+
+
+            var resultString = "";
+
+            using (var httpResponse = await _httpClient.GetAsync($"{_config.ServiceUrl}/api/basket"))
+            {
+                resultString = await httpResponse.Content.ReadAsStringAsync();
+            }
+
+            if (string.IsNullOrEmpty(resultString))
+                return null;
+
+            var result = JsonConvert.DeserializeObject<Basket>(resultString);
+
+            return result;
+        }
     }
 }
